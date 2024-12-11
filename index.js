ec3367edf3dd53fabfa7d06f17274681892085cc
Markos-Souchlos <markosouxlos@gmail.com>
2024-12-12 01:32:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+
+app.get("/", (req,res) => {
+    res.render("home.ejs")
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+});
